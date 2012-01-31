f4c0dddbc37ddad51ae86b5d4092049bb266eea1
Brandon Landry <sirjava@gmail.com>
2012-01-30 22:41:14 -0700

    Added a test.php for easy testing

---
+++ test.php
@@ -0,0 +1,39 @@
+<?php
+
+include 'Bash.php';
+
+$bash = new Bash(); 
+
+$stdout_cb = function($line) { 
+    // actually the last stdout line is Bash::STDOUT_EOF/return_status
+    // which is why strpos is better than an exact string compare
+    if(strpos($line,Bash::STDOUT_EOF) !== 0) {
+        print "<<stdout callback>>  $line\n";
+    }
+};
+
+$stderr_cb = function($line) { 
+    if(strpos($line,Bash::STDERR_EOF) !== 0) {
+        print "<<stderr callback>>  $line\n";
+    }
+};
+
+# demo callback
+$result = $bash->run('echo "Hello from Bash()"',false,$stdout_cb,$stderr_cb);
+spew($result);
+$result = $bash->run('ls fileNotFound',false,$stdout_cb,$stderr_cb);
+spew($result);
+
+# vanilla run
+$result = $bash->run('echo "vanilla run simple output"');
+spew($result);
+# background run (no output)
+$result = $bash->run('sleep 10',true);
+spew($result);
+
+function spew($result){
+    print "(status) '". $result['status'] ."'\n";
+    print "(command)'". $result['command'] ."'\n";
+    print "(stdout) '". implode("\n",$result['output']) ."'\n";
+    print "(stderr) '". implode("\n",$result['error']) ."'\n";
+}
